21860e21acc62fc0a1d4b25d305bd71c7644a460
Santhosh Vodnala <Santhosh.Vodnala@iiitb.ac.in>
2025-05-19 06:52:08 +0530

    counter using useState

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -1,15 +1,24 @@
-import List from './List.jsx'
+import { useState } from "react";
+import MyButton from "./myButton";
+
+function incrementCount({ count, setCount }) {
+  setCount(count + 1);
+}
+function decrementCount({ count, setCount }) {
+  setCount(count - 1);
+}
+function resetCount({ count, setCount }) {
+  setCount(0);
+}
 
 function App() {
-  const fruits = [{id: 1, name:"Apple"},
-                  {id: 2, name:"Strawberry"},
-                  {id: 3, name:"Banana"}];
-  
-  const veggies = [];
+  const [count, setCount] = useState(0);
   return (
     <>
-      <List category="Fruits" items={fruits}/>
-      {veggies.length > 0 && <List category="Veggies" items={veggies}/>}
+      <h1>{count}</h1>
+      <MyButton title="Increase" onclick={() => incrementCount({count, setCount})} />
+      <MyButton title="Decrease" onclick={() => decrementCount({count, setCount})} />
+      <MyButton title="Reset" onclick={() => resetCount({count, setCount})} />
     </>
   );
 }
---
+++ learning-react/src/myButton.jsx
@@ -0,0 +1,5 @@
+function MyButton({ title, onclick }) {
+  return <button onClick={onclick}>{title}</button>;
+}
+
+export default MyButton;
