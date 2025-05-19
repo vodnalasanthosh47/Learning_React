6831209e29c66a2078d36a63910f593d9350255d
Santhosh Vodnala <Santhosh.Vodnala@iiitb.ac.in>
2025-05-19 06:58:49 +0530

    reorganisation

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -1,26 +1,5 @@
-import { useState } from "react";
-import MyButton from "./myButton";
-
-function incrementCount({ count, setCount }) {
-  setCount(count + 1);
-}
-function decrementCount({ count, setCount }) {
-  setCount(count - 1);
-}
-function resetCount({ count, setCount }) {
-  setCount(0);
-}
-
 function App() {
-  const [count, setCount] = useState(0);
-  return (
-    <>
-      <h1>{count}</h1>
-      <MyButton title="Increase" onclick={() => incrementCount({count, setCount})} />
-      <MyButton title="Decrease" onclick={() => decrementCount({count, setCount})} />
-      <MyButton title="Reset" onclick={() => resetCount({count, setCount})} />
-    </>
-  );
+  
 }
 
 export default App;
